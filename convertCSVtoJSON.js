1f7e35f9fee85a133534bad8ecff765211cb7b4e
Arvind Shelke <arvind.shelke@devdynamics.ai>
2023-11-08 15:27:11 +0530

    Update convertCSVtoJSON.js

--- convertCSVtoJSON.js
+++ convertCSVtoJSON.js
@@ -3,6 +3,7 @@ const csv = require("csvtojson");
 
 // Function to sort the CSV data by the second column (Email)
 function sortByEmail(a, b) {
+  
   if (a.Email < b.Email) return -1;
   if (a.Email > b.Email) return 1;
   return 0;
@@ -13,6 +14,7 @@ axios
   .get("https://coderbyte.com/api/challenges/logs/user-info-csv")
   .then((response) => {
     // Parse CSV data
+    
     csv()
       .fromString(response.data)
       .then((csvData) => {
@@ -22,6 +24,7 @@ axios
         // Convert the sorted CSV data to a JSON object
         const jsonData = JSON.stringify(csvData, null, 2);
 
+        
         // Console log the JSON object as a string
         console.log(jsonData);
       })
